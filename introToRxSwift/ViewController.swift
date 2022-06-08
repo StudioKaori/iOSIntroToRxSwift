be5740b144ff74151eb1e04bbb9914e0e500e2a0
Studio Kaori <studiokaori.com@gmail.com>
2022-06-08 14:46:09 +0200

    Add: Product and ProductViewModel

--- introToRxSwift/ViewController.swift
+++ introToRxSwift/ViewController.swift
@@ -6,8 +6,40 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+struct Product {
+    let imageName: String
+    let title: String
+}
+
+struct ProductViewMode {
+    // take generic
+    var items = PublishSubject<[Product]>()
+    
+    func fetchItems() {
+        let products = [
+            Product(imageName: "house", title: "Home"),
+            Product(imageName: "gear", title: "Settings"),
+            Product(imageName: "person.circle", title: "Profile"),
+            Product(imageName: "airplane", title: "Flights"),
+            Product(imageName: "bell", title: "Activity")
+        ]
+        
+        items.onNext(products)
+        items.onCompleted()
+    }
+}
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
